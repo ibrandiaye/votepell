e9e512d57949bf2e0ec49e6635f343890c521e8c
ibrandiaye <ibra789ndiaye@gmail.com>
2025-09-30 11:37:08 +0000

    dddddddddd

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Système de Vote en Ligne</title>
+
+            <link rel="shortcut icon" href="{{ asset('photo/pell.png') }}">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome pour les icônes -->
